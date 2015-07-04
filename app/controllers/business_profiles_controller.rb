4d72aac26c2ce3d4c038b5388410483372225a6b
Abi Tyas Tunggal <Abi@tyastunggal.com>
2015-07-04 14:39:04 +1000

    build profile when user created

--- app/controllers/business_profiles_controller.rb
+++ app/controllers/business_profiles_controller.rb
@@ -77,7 +77,7 @@ class BusinessProfilesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_business_profile
-      @business_profile = BusinessProfile.find(params[:id])
+      @business_profile = BusinessProfile.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
